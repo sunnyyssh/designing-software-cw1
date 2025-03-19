08c17a987ce8e2119437f40c7b87869a8c714904
sunnyyssh <blit228@mail.ru>
2025-03-19 19:57:49 +0300

    Add cobra CLI

---
+++ internal/config/db.go
@@ -0,0 +1,4 @@
+package config
+
+type DB struct {
+}
---
+++ internal/config/services.go
@@ -0,0 +1,7 @@
+package config
+
+import "github.com/sunnyyssh/designing-software-cw1/internal/application/services"
+
+type Services struct {
+	BankService services.BankService
+}
